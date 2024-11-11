cddc391f39ed443be5a531dc842da793714a7d6b
Joshua Davian <joshuadavian@gmail.com>
2024-11-11 20:04:32 +0700

    feat: Payment Method Model & Migration

---
+++ app/Models/PaymentMethod.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class PaymentMethod extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'image',
+        'is_cash',
+        'is_active'
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'is_cash' => 'boolean',
+            'is_active' => 'boolean'
+        ];
+    }
+}
