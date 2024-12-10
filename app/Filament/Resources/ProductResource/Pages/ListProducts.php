e6093c2231b41d99d1733173e9be7d45a8d025c2
Joshua Davian <joshuadavian@gmail.com>
2024-12-10 21:57:14 +0700

    feat: Product Import Form Modal

--- app/Filament/Resources/ProductResource/Pages/ListProducts.php
+++ app/Filament/Resources/ProductResource/Pages/ListProducts.php
@@ -4,7 +4,9 @@ namespace App\Filament\Resources\ProductResource\Pages;
 
 use App\Filament\Resources\ProductResource;
 use Filament\Actions;
+use Filament\Forms\Components\FileUpload;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Support\HtmlString;
 
 class ListProducts extends ListRecords
 {
@@ -13,6 +15,16 @@ class ListProducts extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
+            Actions\Action::make('Import')
+                ->color('violet')
+                ->label('Import Product')
+                ->icon('heroicon-m-arrow-up-tray')
+                ->form([
+                    FileUpload::make('product_import_file')
+                        ->label('Product Import File')
+                        ->helperText(new HtmlString('Download the import template <a href="' . route('products.export-product-template') . '" style="font-weight: bold; color: #8b5cf6;">here</a>'))
+                        ->required()
+                ]),
             Actions\CreateAction::make()
                 ->label('New Product')
                 ->icon('heroicon-m-plus'),
