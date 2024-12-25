36d62910b3c140fcab96effa407a58fffe02d107
Joshua Davian <joshuadavian@gmail.com>
2024-12-25 17:21:47 +0700

    feat: Get All Payment Methods API

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\PaymentMethodController;
 use App\Http\Controllers\Api\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -10,6 +11,8 @@ Route::post('login', [AuthController::class, 'login']);
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::apiResource('products', ProductController::class)->only(['index']);
     Route::get('products/get-product-by-barcode', [ProductController::class, 'getProductByBarcode']);
+
+    Route::apiResource('payment-methods', PaymentMethodController::class)->only(['index']);
 });
 
 Route::get('/user', function (Request $request) {
