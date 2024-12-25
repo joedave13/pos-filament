0ea95ed99a43417dad0bd4d2ffc302a449b6b9b3
Joshua Davian <joshuadavian@gmail.com>
2024-12-25 17:06:47 +0700

    feat: Get Products & Get Product By Barcode API

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,16 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('login', [AuthController::class, 'login']);
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::apiResource('products', ProductController::class)->only(['index']);
+    Route::get('products/get-product-by-barcode', [ProductController::class, 'getProductByBarcode']);
+});
 
 Route::get('/user', function (Request $request) {
     return $request->user();
